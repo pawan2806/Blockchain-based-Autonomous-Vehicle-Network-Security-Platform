6a908016b0dce1e99ca980426a98c8ed14350b7d
Pawan <pawan.singh.co19@nsut.ac.in>
2022-09-28 02:55:34 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataviewComponent } from './viewdata/dataview/dataview.component';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 
+
+// import {ViewdataModule} from './viewdata/viewdata.module';
+// import {DataviewComponent} from './viewdata/da';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataviewComponent,
+    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    BrowserModule, 
+    ReactiveFormsModule ,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/viewdata/viewdata.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataviewComponent } from './dataview/dataview.component';
+
+
+
+@NgModule({
+  declarations: [
+    DataviewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ViewdataModule { }
